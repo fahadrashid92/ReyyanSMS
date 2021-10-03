db6f7a534f0bbe97dc75deb91f7b653381b63de5
fahadrashid92 <55449188+fahadrashid92@users.noreply.github.com>
2021-10-03 22:55:31 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask,jsonify,render_template
+import requests
+import json
+import datetime
+import pyodbc
+
+
+app = Flask(__name__, template_folder='template')
+
+@app.route("/Verify")
+def Verify():
+    
+    return "Lead Endpoint Response Saved successfully"     
+
+
+
+@app.route("/Scrapper")
+def scrapper():
+    return "Data ETL is running..."
+
+@app.route("/")
+def landing():
+    return "Data ETL is running..."
+
+if __name__ == "__main__":
+    app.run()
